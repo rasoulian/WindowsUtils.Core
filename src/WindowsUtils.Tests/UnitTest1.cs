dead6aff4b0aed66dd890b5b8f4e21fa7d0e1c49
rasoulian <arasoulian@gmail.com>
2019-01-19 11:33:28 +0330

    add tests

---
+++ src/WindowsUtils.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsUtils.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
